8e54052c6eda70f413d1459d6aad2a17f4542271
markitosha <markitosha@gmail.com>
2025-08-25 15:00:40 +0200

    Removed unused file

--- src/dev/devEnvironment.ts
+++
@@ -1,5 +0,0 @@
-export const devEnvironment = {
-    base: `https://api.${process.env.NEXT_PUBLIC_ENVIRONMENT_ID}.corti.app/v2`,
-    wss: `wss://api.${process.env.NEXT_PUBLIC_ENVIRONMENT_ID}.corti.app`,
-    login: `https://auth.${process.env.NEXT_PUBLIC_ENVIRONMENT_ID}.corti.app/realms`,
-}
