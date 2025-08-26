5ed505b669fc4ab9005fe8de1bc82dcaa09d89f2
markitosha <markitosha@gmail.com>
2025-08-26 09:59:42 +0200

    Updated examples from doc

--- src/app/api/examples/corti-docs/full/route.js
+++ src/app/api/examples/corti-docs/full/route.js
@@ -1,86 +1,51 @@
 import { NextResponse } from 'next/server';
 import { CortiClient, CortiEnvironment } from '@corti/sdk';
-import * as rw from 'fs';
+import fs from 'fs';
 
 export async function GET() {
     try {
-        function startClient(){
-            const client = new CortiClient({
-                environment: process.env.NEXT_PUBLIC_ENVIRONMENT_ID === 'eu'
-                    ? CortiEnvironment.Eu
-                    : process.env.NEXT_PUBLIC_ENVIRONMENT_ID,
-                auth: {
-                    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-                    clientSecret: process.env.CLIENT_SECRET,
-                },
-                tenantName: process.env.NEXT_PUBLIC_TENANT_NAME,
-            });
+        const {
+            NEXT_PUBLIC_CLIENT_ID: CLIENT_ID,
+            CLIENT_SECRET,
+            NEXT_PUBLIC_TENANT_NAME: TENANT
+        } = process.env;
 
-            return client;
-        }
+        const client = new CortiClient({
+            environment: CortiEnvironment.Eu,
+            auth: { clientId: CLIENT_ID, clientSecret: CLIENT_SECRET },
+            tenantName: TENANT,
+        });
 
-        async function startInteraction(client){
-            let intRes = await client.interactions.create({
-                encounter: {
-                    identifier: 'GUID OF YOUR CHOOSING 8',
-                    status: 'planned',
-                    type: 'first_consultation'
-                }
-            });
-            return intRes;
-        }
+        const { interactionId } = await client.interactions.create({
+            encounter: {
+                identifier: 'GUID OF YOUR CHOOSING',
+                status: 'planned',
+                type: 'first_consultation',
+            },
+        });
 
-        async function uploadRecording(client, interactId){
-            const file = rw.createReadStream('public/trouble-breathing.mp3', { autoClose: true });
-            let res = await client.recordings.upload(file, interactId);
-            return res;
-        }
+        const { recordingId } = await client.recordings.upload(
+            fs.createReadStream('public/trouble-breathing.mp3', { autoClose: true }),
+            interactionId
+        );
 
-        async function generateTranscript(client, recordingId, interactId){
-            let transcriptResult = await client.transcripts.create(interactId, {
-                recordingId: recordingId,
-                primaryLanguage: 'en',
-                modelName: 'premier'
-            });
-            return transcriptResult;
-        }
+        const transcript = await client.transcripts.create(interactionId, {
+            recordingId,
+            primaryLanguage: 'en',
+            modelName: 'premier',
+        });
 
-        async function generateDocument(client, interactId, transcriptResult){
-            let documentResult = await client.documents.create(interactId, {
-                context: [{
-                    type: 'transcript',
-                    data: transcriptResult.transcripts[0]
-                }],
-                templateKey: 'soap',
-                outputLanguage: 'en'
-            });
-            return documentResult;
-        }
-
-        async function getDocument(client, interactId, documentId){
-            let document = await client.documents.get(interactId, documentId);
-            return document;
-        }
-
-        async function workflow(){
-            const clientPassed = startClient();
-            const interactionResult = await startInteraction(clientPassed);
-            const uploadResult = await uploadRecording(clientPassed, interactionResult.interactionId);
-            const transcriptResult = await generateTranscript(clientPassed, uploadResult.recordingId, interactionResult.interactionId);
-            const documentResult = await generateDocument(clientPassed, interactionResult.interactionId, transcriptResult);
-            const document = await getDocument(clientPassed, interactionResult.interactionId, documentResult.id);
-
-            return {
-                interaction: interactionResult,
-                recording: uploadResult,
-                transcript: transcriptResult,
-                documentCreation: documentResult,
-                document: document
-            }
-        }
+        const document = await client.documents.create(interactionId, {
+            context: [{
+                type: 'string',
+                data: transcript.transcripts.reduce((context, { text }) => context + ' ' + text, '')
+            }],
+            templateKey: 'soap',
+            outputLanguage: 'en',
+        });
 
         return NextResponse.json({
-            data: await workflow()
+            data: document
         });
     } catch (error) {
         console.log(error);
--- src/app/api/examples/corti-docs/interaction/route.js
+++ src/app/api/examples/corti-docs/interaction/route.js
@@ -3,42 +3,25 @@ import { CortiClient, CortiEnvironment } from '@corti/sdk';
 
 export async function GET() {
     try {
-        function startClient(){
-            const client = new CortiClient({
-                environment: process.env.NEXT_PUBLIC_ENVIRONMENT_ID === 'eu'
-                    ? CortiEnvironment.Eu
-                    : process.env.NEXT_PUBLIC_ENVIRONMENT_ID,
-                auth: {
-                    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
-                    clientSecret: process.env.CLIENT_SECRET,
-                },
-                tenantName: process.env.NEXT_PUBLIC_TENANT_NAME,
-            });
+        const client = new CortiClient({
+            environment: CortiEnvironment.Eu,
+            auth: {
+                clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+            },
+            tenantName: process.env.NEXT_PUBLIC_TENANT_NAME,
+        });
 
-            return client;
-        }
-
-        async function startInteraction(client) {
-            const intRes = await client.interactions.create({
-                encounter: {
-                    identifier: 'YOUR_IDENTIFIER 2',
-                    status: 'planned',
-                    type: 'first_consultation'
-                }
-            });
-
-            return intRes;
-        }
-
-        async function workflow(){
-            const clientPassed = startClient();
-            const interactionResult = await startInteraction(clientPassed);
-
-            return interactionResult;
-        }
+        const { interactionId } = await client.interactions.create({
+            encounter: {
+                identifier: 'YOUR_IDENTIFIER',
+                status: 'planned',
+                type: 'first_consultation'
+            }
+        });
 
         return NextResponse.json({
-            data: await workflow()
+            data: interactionId
         });
     } catch (error) {
         return NextResponse.json({
